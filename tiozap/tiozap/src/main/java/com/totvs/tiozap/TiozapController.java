2d210a9c0e2850d0dab783c39d027b1b3143db15
Daiana Rodrigues Nascimento <102737592+DaianaRodrigues@users.noreply.github.com>
2023-03-06 14:40:36 -0300

    Add files via upload

---
+++ tiozap/tiozap/src/main/java/com/totvs/tiozap/TiozapController.java
@@ -0,0 +1,20 @@
+package com.totvs.tiozap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDate;
+
+@RestController
+public class TiozapController {
+    @GetMapping("/")
+    public String dizerHello() {
+
+        return "Hello Kitty";
+    }
+
+    @GetMapping("/data")
+    public LocalDate mostrarData() {
+
+        return LocalDate.now();
+    }
+}
